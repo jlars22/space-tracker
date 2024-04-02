970d9299b9c73d243c95dd26c876f16cc4fe0207
jlars22 <jonas.larsen@cardlay.com>
2024-04-02 09:24:18 +0200

    setup frontend again

--- frontend/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <div className="text-red-600">Hey</div>
-  </React.StrictMode>
-);
